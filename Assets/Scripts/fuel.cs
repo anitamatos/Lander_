d88de446dd494914842d951f0b69bf407db94660
anitamatos <40200233@esmad.ipp.pt>
2021-04-13 19:37:18 +0100

    Initial commit

---
+++ Assets/Scripts/fuel.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fuel : MonoBehaviour
+{
+    [SerializeField]
+    float combustivel = 500f;
+
+    [SerializeField]
+    float turnVelocity = 1f;
+    [SerializeField]
+    float thurstVelocity = 1f;
+    [SerializeField]
+    float combustivelGasto = 10f;
+    [SerializeField]
+    float combustivelGastoTorque = 5f;
+    public Rigidbody2D RigidBody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (combustivel > 0)
+            {
+                RigidBody.AddForce(transform.up * thurstVelocity * Time.deltaTime);
+                combustivel -= combustivelGasto * Time.deltaTime;
+                Debug.Log(combustivel.ToString());
+            }
+
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (combustivel > 0)
+            {
+                RigidBody.AddTorque(-turnVelocity * Time.deltaTime);
+                combustivel -= combustivelGastoTorque * Time.deltaTime;
+                Debug.Log(combustivel.ToString());
+            }
+
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (combustivel > 0)
+            {
+                RigidBody.AddTorque(turnVelocity * Time.deltaTime);
+                combustivel -= combustivelGastoTorque * Time.deltaTime;
+                Debug.Log(combustivel.ToString());
+            }
+        }
+        if (combustivel <= 0 && combustivel > -1)
+        {
+            Debug.Log("GAME OVER");
+
+        }
+
+    }
+
+}
+
---
+++ Assets/Scripts/ship.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ship : MonoBehaviour
+{
+    [SerializeField]
+    float maxRelativeVelocity = 2f;
+
+    [SerializeField]
+    float maxRotation = 10f;
+
+    [SerializeField]
+    float thrustForce = 150f;
+
+    [SerializeField]
+    float torqueForce = 15f;
+
+    void Update()
+    {
+        // Clicar as duas teclas ao mesmo tempo 
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(transform.up * thrustForce * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().AddTorque(torqueForce * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().AddTorque(-torqueForce * Time.deltaTime);
+        }
+    }
+
+
+    // Como saber se bati?
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Plataforma") // Quando toca na plataforma
+        {
+            Debug.Log("Aterrei"); //-> Só para ver como funciona, que aterra corretamente 
+            Debug.Log(collision.relativeVelocity);
+            if (collision.relativeVelocity.magnitude > maxRelativeVelocity || Mathf.Abs(transform.localEulerAngles.z) > maxRotation)
+            {
+                Debug.Log("...Mas Expoldi");
+            }
+
+        }
+        else
+        {
+            Debug.Log("Expoldir");    // Quando toca na Lua
+        }
+    }
+}
+
+
